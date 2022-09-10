b5228d2944a2e068402852d63b45277fa19ccef6
Vladislav ST <adrenaline2142@gmail.com>
2022-09-10 10:49:05 +0500

    fix

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,6 @@
 /* eslint-disable curly */
 import {iosVhFix} from './utils/ios-vh-fix';
 import {swap} from './modules/modals';
-import {readMore} from './modules/about';
 import {initFooter} from './modules/footer';
 
 // ---------------------------------
@@ -12,6 +11,32 @@ window.addEventListener('DOMContentLoaded', () => {
   // ---------------------------------
 
   iosVhFix();
+  const txtHideDesktop = document.querySelector('.introduction__txt-desk');
+  const txtHideMobile = document.querySelector('.introduction__txt-mob');
+  const introductionbtn = document.querySelector('.introduction__btn');
+
+  txtHideDesktop.classList.remove('introduction__txt-desk--nojs');
+  txtHideMobile.classList.remove('introduction__txt-mob--nojs');
+
+  const readMore = () => {
+    if (txtHideDesktop.classList.contains('introduction__txt-desk--hidden')) {
+      txtHideDesktop.classList.remove('introduction__txt-desk--hidden');
+      introductionbtn.innerHTML = 'Скрыть';
+    } else {
+      txtHideDesktop.classList.add('introduction__txt-desk--hidden');
+      introductionbtn.innerHTML = 'Подробнее';
+    }
+
+    if (txtHideMobile.classList.contains('introduction__txt-mob--hidden')) {
+      txtHideMobile.classList.remove('introduction__txt-mob--hidden');
+      introductionbtn.innerHTML = 'Скрыть';
+    } else {
+      txtHideMobile.classList.add('introduction__txt-mob--hidden');
+      introductionbtn.innerHTML = 'Подробнее';
+    }
+  };
+
+  introductionbtn.addEventListener('click', readMore);
 
   // initFormValidation();
 
@@ -21,7 +46,6 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    readMore();
     initFooter();
     swap();
   });
